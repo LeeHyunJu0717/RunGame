468059e9cea55d1327925e8ee76de02749a43892
LeeHyunJu0717 <78850239+LeeHyunJu0717@users.noreply.github.com>
2021-02-10 20:55:13 +0900

    초기 파일

---
+++ Assets/2_Scripts/Managers/SceneControlManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+using System;
+
+public class SceneControlManager : MonoBehaviour
+{
+    enum SceneType
+    {
+        MAIN,
+        INGAME
+    }
+
+    public enum LoadingState
+    {
+        none = 0,
+
+        LoadSceneStart,
+        LoadingScene,
+        LoadSceneEnd,
+
+        LoadStageStart,
+        LoadingStage,
+        LoadStageEnd,
+        LoadEnd
+    }
+
+    SceneType _nowSceneType;
+    SceneType _oldSceneType;
+    LoadingState _currentStateLoad;
+
+    static SceneControlManager _uniqueInstance;
+
+    public static SceneControlManager _instance
+    {
+        get { return _uniqueInstance; }
+    }
+
+    private void Awake()
+    {
+        _uniqueInstance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    IEnumerator LoadingScene(string SceneName)
+    {
+        AsyncOperation async;
+        Scene aScene;
+
+        yield return null;
+    }
+}
